29963cf7c3fb26ec0fe97a8bcd9416de3e4c4fa4
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-08-15 20:00:13 +0600

    Complete PositiveNegativeZero

---
+++ 02-expressions-statements/Challanges/src/PositiveNegativeZero.java
@@ -0,0 +1,15 @@
+public class PositiveNegativeZero {
+
+    public static void checkNumber(int number) {
+
+        if(number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+
+        System.out.println((number > 0) ? "positive" : (number < 0) ? "negative" : "zero");
+    }
+}
