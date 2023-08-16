fd2a5b9e258a37550c61b752c511b8cdd60f5dc8
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-08-16 11:50:12 +0600

    Complete LeapYear

---
+++ 02-expressions-statements/Challanges/src/LeapYear.java
@@ -0,0 +1,13 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(2044));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
